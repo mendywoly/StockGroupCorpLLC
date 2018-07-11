b1e510587960bc6521a96a8b2e95d4558d65bcb7
Mendy Wolosow <mendywoly@gmail.com>
2018-07-11 13:18:35 -0400

    Added forced login, only acces your account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,30 @@ class UsersController < ApplicationController
     def show
         if(requires_login)
             @user = User.find(params[:id])
-            render json: @user
+            payload = decode_token
+            h = User.find_by(name: payload[0]['name'])
+                if h.id === @user.id
+                render json: @user 
+                else
+                    render json: {message: "No!"}
+                end
         else
             render json: {message: "No!"}
         end
+    end
+
+    def create
+        # byebug
+        user = User.create(name: params[:name], username: params[:username], password: params[:password])
+        if user.valid?
+            render json: {
+                user: user,
+                token: get_token(payload( user.name, user.id ))
+            }
+        else
+            render json: user.errors
+        end
+                
     end
 
 end
