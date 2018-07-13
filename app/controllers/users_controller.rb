8ef0683ebc549ed395a06cd82089d45a590b5411
Mendy Wolosow <mendywoly@gmail.com>
2018-07-13 09:24:20 -0400

    Fixed update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,20 +23,25 @@ class UsersController < ApplicationController
     end
 
     def update
-        # byebug
         if(requires_login && authorized?(params[:id]))
-            user = User.find(params[:id])
-            stock = StockSymbol.find_by(stock_symbol: params[:symbolId])
-            user.stock_symbols << stock
-            render json: stock
+            if  params[:type] == 'removeFavorite'
+                user = User.find(params[:id])
+                stock = StockSymbol.find_by(stock_symbol: params[:symbol])
+                user.stock_symbols.delete(stock)
+                # Favorite.where(user_id: user.id).where(stock_symbol)
+                # byebug
+                # stock = user.stock_symbols.where(stock_symbol: params[:symbol])
+                render json: {message: "Gone"}
+            else
+                user = User.find(params[:id])
+                stock = StockSymbol.find_by(stock_symbol: params[:symbolId])
+                user.stock_symbols << stock
+                render json: stock
+            end
         else
             render json: {Err: "user.errors"}
         end
-
-
-
     end
-
     
 
 end
